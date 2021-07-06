3ab76ff7847122def590377e498dc131d1617f1b
shamichiu <handsomeno7@gmail.com>
2021-07-06 10:40:05 +0800

    sticker4

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ def handle_message(event):
     msg = event.message.text
     r = '你在公三小'
 
-    if '操' in msg:
+    if '給我貼圖' in msg:
         sticker_message = StickerSendMessage(
             package_id='1',
-            sticker_id='23'
+            sticker_id='1'
         )
 
         line_bot_api.reply_message(
