f1c888720ee0957be972446f2d2481576c8661fa
shamichiu <handsomeno7@gmail.com>
2021-07-06 10:35:57 +0800

    fix sticker3

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=r))
+            sticker_message)
         return
 
     if msg == 'hi':
